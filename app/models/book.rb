1483e5048ccc9d30d19d31e29be97ddc8c870ac9
Dan Kiss and John Rykken <john@>
2018-04-30 15:56:35 -0700

    Successfully hide API key.

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book
 
 
   def make_NYT_API_call
-    response = RestClient::Request.execute(method: :get, url: 'http://api.nytimes.com/svc/books/v3/lists/names.json', headers: {api_key: 'be5ffc44e6454459a6ccd09b9e9d2e0c'})
+    response = RestClient::Request.execute(method: :get, url: 'http://api.nytimes.com/svc/books/v3/lists/names.json', headers: {api_key: ENV['NYT_API_KEY'] } )
     JSON.parse(response)
   end
 
