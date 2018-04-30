781de7e2e4fa8db4f83cc60a5ea4f1c6f9eff338
Dan Kiss and John Rykken <dan@>
2018-04-30 11:04:33 -0700

    Add navbar.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_order
+  helper_method :current_user
 
   def current_order
     if session[:order_id]
