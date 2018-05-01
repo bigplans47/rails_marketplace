6a527556082ec1cb8b17b53edeb7ffefaf35fece
Dan Kiss <flowfast47@gmail.com>
2018-05-01 16:11:17 -0700

    add way to add books and show them

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,58 @@
 class BooksController < ApplicationController
-
-
-
   def index
-    api_call = Book.new
-    @response = api_call.API_call()
+    @books = Book.all
+    # @availibleBook = Book.where(availible: )
   end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = 'Book added'
+      redirect_to books_path
+    else
+      flash[:alert] = 'Book not added'
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'Book updated'
+      redirect_to books_path
+    else
+      flash[:alert] = 'Book not updated'
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = 'Book removed'
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :availible)
+  end
+
+
+  # def index
+  #   api_call = Book.new
+  #   @response = api_call.API_call()
+  # end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,9 @@
-class Book
+class Book < ApplicationRecord
+  # validates :title, :availible, :presence => true
 
-  def API_call
-    response = RestClient::Request.execute(method: :get, url: 'https://api.nytimes.com/svc/books/v3/lists/combined-print-and-e-book-fiction.json', headers: {api_key: ENV['NYT_API_KEY']})
-    JSON.parse(response)
-  end
+  # def API_call
+  #   response = RestClient::Request.execute(method: :get, url: 'https://api.nytimes.com/svc/books/v3/lists/combined-print-and-e-book-fiction.json', headers: {api_key: ENV['NYT_API_KEY']})
+  #   JSON.parse(response)
+  # end
 
 end
