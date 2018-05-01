74f77bae136c002179447c65ad7b74438e097c0d
Dan Kiss and John Rykken <dan@>
2018-05-01 14:30:40 -0700

    Fix bulma. Add book routes. Begin AJAX.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :products
   resources :order_items
   resource :cart, only: [:show]
-  resource :book, only: [:show]
+  resource :book
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
